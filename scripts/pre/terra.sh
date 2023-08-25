7d833e15fffbbd4b1baaa76e18daa6f9794ae204
Mark Mihalik <markamihalik@gmail.com>
2023-08-25 12:47:53 -0400

    fix: justfile.

--- scripts/pre/terra.sh
+++ scripts/pre/terra.sh
@@ -5,4 +5,4 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-wget https://terra.fyralabs.com/terra.repo -O /etc/yum.repos.d/terra.repo
+#wget https://terra.fyralabs.com/terra.repo -O /etc/yum.repos.d/terra.repo
