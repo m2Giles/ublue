5bd07459d2ee48213c946c1de833e7a635119cfd
Mark Mihalik <markamihalik@gmail.com>
2023-10-19 11:13:12 -0400

    Remove Virtualization

--- scripts/post/pano.sh
+++ scripts/post/pano.sh
@@ -3,7 +3,7 @@
 # Tell this script to exit if there are any errors.
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
-set -oue pipefail
-
-wget https://github.com/oae/gnome-shell-pano/releases/latest/download/pano@elhan.io.zip -O /tmp/pano@elhan.io.zip
-unzip /tmp/pano@elhan.io.zip -d /usr/share/gnome-shell/extensions/pano@elhan.io
+# set -oue pipefail
+# 
+# wget https://github.com/oae/gnome-shell-pano/releases/latest/download/pano@elhan.io.zip -O /tmp/pano@elhan.io.zip
+# unzip /tmp/pano@elhan.io.zip -d /usr/share/gnome-shell/extensions/pano@elhan.io
