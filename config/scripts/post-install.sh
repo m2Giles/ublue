c69baa3971d7f726832889cf59d61bd91293acd1
Mark Mihalik <markamihalik@gmail.com>
2023-10-31 17:04:19 -0400

    Update repos. Place imageflavor in signing.sh

--- config/scripts/post-install.sh
+++ config/scripts/post-install.sh
@@ -11,9 +11,9 @@ rm -f /usr/share/applications/nvtop.desktop
 rm -f /usr/share/applications/org.gnome.Extensions.desktop
 
 # Remove repo files
-rm -f /etc/yum.repos.d/**
+#rm -f /etc/yum.repos.d/**
 
 # Speedup Shutdown and style os-release
-sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf
-sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=30s/' /etc/systemd/user.conf
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=30s/' /etc/systemd/system.conf
 sed -i '/^PRETTY_NAME/s/Silverblue/m2-ublue/' /usr/lib/os-release
--- config/scripts/signing.sh
+++ config/scripts/signing.sh
@@ -10,6 +10,8 @@ cp /usr/share/ublue-os/cosign.pub /usr/etc/pki/containers/"$IMAGE_NAME".pub
 
 FILE=/usr/etc/containers/policy.json
 
+IAMGE_FLAVOR="$(yq '.flavor' "$CONFIG_DIRECTORY/$RECIPE")"
+
 yq -i -o=j '.transports.docker |=
     {"'"$IMAGE_REGISTRY"'/'"$IMAGE_NAME"'": [
             {
@@ -24,7 +26,7 @@ yq -i -o=j '.transports.docker |=
 + .' "$FILE"
 
 IMAGE_REF="ostree-image-signed:docker://$IMAGE_REGISTRY/$IMAGE_NAME"
-printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest"\n, \n"image-flavor": "'"$IMAGE_FLAVOR"'"\n}' > /usr/share/ublue-os/image-info.json
+printf '{\n"image-ref": "'"$IMAGE_REF"'",\n"image-tag": "latest",\n"image-flavor": "'"$IMAGE_FLAVOR"'"\n}' > /usr/share/ublue-os/image-info.json
 
 cp /usr/etc/containers/registries.d/ublue-os.yaml /usr/etc/containers/registries.d/"$IMAGE_NAME".yaml
 sed -i "s ghcr.io/ublue-os $IMAGE_REGISTRY g" /usr/etc/containers/registries.d/"$IMAGE_NAME".yaml
