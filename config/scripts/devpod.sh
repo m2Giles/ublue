066b58d1c3fa0fca4c58342e114194254a4b3481
Mark Mihalik <markamihalik@gmail.com>
2023-11-02 12:02:24 -0400

    add flags for copy

--- config/scripts/devpod.sh
+++ config/scripts/devpod.sh
@@ -9,8 +9,8 @@ wget https://github.com/loft-sh/devpod/releases/latest/download/DevPod_linux_x86
 mkdir /tmp/devpod-package
 tar -xvf /tmp/devpod.tar.gz -C /tmp/devpod-package
 cp /tmp/devpod-package/usr/bin/{dev-pod,devpod-cli} /usr/bin/
-cp /tmp/devpod-package/usr/share/applications/** /usr/share/applications/
-cp /tmp/devpod-package/usr/share/glib-2.0/** /usr/share/glib-2.0/
-cp /tmp/devpod-package/usr/share/icons/** /usr/share/icons/
+cp -r /tmp/devpod-package/usr/share/applications/** /usr/share/applications/
+cp -r /tmp/devpod-package/usr/share/glib-2.0/** /usr/share/glib-2.0/
+cp -r /tmp/devpod-package/usr/share/icons/** /usr/share/icons/
 curl -L -o /tmp/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64"
 install -c -m 0755 /tmp/devpod /usr/bin
