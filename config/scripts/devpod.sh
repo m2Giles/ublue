f87d364ef6dc41da7e96de0916bf18bca2617321
Mark Mihalik <markamihalik@gmail.com>
2023-11-02 11:45:56 -0400

    workaround devpod rpm.

--- config/scripts/devpod.sh
+++ config/scripts/devpod.sh
@@ -6,4 +6,11 @@
 set -oue pipefail
 
 wget https://github.com/loft-sh/devpod/releases/latest/download/DevPod_linux_x86_64.tar.gz -O /tmp/devpod.tar.gz
-tar -xvf /tmp/devpod.tar.gz -C /
+mkdir /tmp/devpod-package
+tar -xvf /tmp/devpod.tar.gz -C /tmp/devpod
+cp /tmp/devpod-package/usr/bin/{dev-pod,devpod-cli} /usr/bin/
+cp /tmp/devpod-package/usr/share/applications/** /usr/share/applications/
+cp /tmp/devpod-package/usr/share/glib-2.0/** /usr/share/glib-2.0/
+cp /tmp/devpod-package/usr/share/icons/** /usr/share/icons/
+curl -L -o /tmp/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64"
+install -c -m 0755 /tmp/devpod /usr/bin
