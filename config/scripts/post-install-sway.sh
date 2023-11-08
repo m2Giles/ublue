80528e87c3225ff311e21d747515d1a5c3951a45
Mark Mihalik <markamihalik@gmail.com>
2023-11-08 12:41:35 -0500

    Make a sericea build

---
+++ config/scripts/post-install-sway.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Remove Unnecessary desktop entries from overview
+rm -f /usr/share/applications/htop.desktop
+rm -f /usr/share/applications/nvtop.desktop
+rm -f /usr/share/applications/org.gnome.Extensions.desktop
+
+# Remove repo files
+rm -f /etc/yum.repos.d/**
+
+# mv over /etc files
+cp -r "/var/lib/alternatives" "/usr/share/alternatives"
+
+# Starship in /etc/bashrc
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
+
+# Symlink Yafti
+ln -s /usr/share/ublue-os/firstboot/yafti.yml /etc/yafti.yml
+
+# Speedup Shutdown and style os-release
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=30s/' /etc/systemd/user.conf
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=30s/' /etc/systemd/system.conf
+sed -i '/^PRETTY_NAME/s/Sericea/m2-ublue-sericea/' /usr/lib/os-release
