7d72bc568af32285ad3bdcf2ce5e33b5c046e9aa
m2Giles <69128853+m2Giles@users.noreply.github.com>
2023-11-09 13:58:50 -0500

    Move chrome to flatpak to avoid font issue.

---
+++ modules/yafti-google-chrome/yafti-google-chrome.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+YAFTI_FILE="$FIRSTBOOT_DATA/yafti.yml"
+
+get_yaml_array FLATPAKS '.google-chrome-flatpak[]' "$1"
+if [[ ${#FLATPAKS[@]} -gt 0 ]]; then
+    echo "Adding Google Chrome to yafti.yml"
+    yq -i '.screens.applications.values.groups.Google Chrome.description = "Install Google Chrome."' "${YAFTI_FILE}"
+    yq -i '.screens.applications.values.groups.Google Chrome.default = true' "${YAFTI_FILE}"
+
+    for pkg in "${FLATPAKS[@]}"; do
+        echo "Adding to yafti: ${pkg}"
+        yq -i ".screens.applications.values.groups.Google Chrome.packages += [$pkg]" "${YAFTI_FILE}"
+    done
+fi
