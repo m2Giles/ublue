cbf2677f702a34b84957aef60477502610088833
Mark Mihalik <markamihalik@gmail.com>
2023-11-09 11:45:59 -0500

    add items to sway image.

--- config/scripts/early-script.sh
+++ config/scripts/early-script.sh
@@ -9,3 +9,4 @@ curl --retry 3 --retry-delay 2 --retry-all-errors -sL \
   -o /etc/pki/rpm-gpg/RPM-GPG-KEY-google \
   https://dl.google.com/linux/linux_signing_key.pub
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-google
+rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
