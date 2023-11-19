a5493e288145cf3ede1a40f0d3835320efe05e7a
Mark Mihalik <markamihalik@gmail.com>
2023-11-19 17:45:02 -0500

    reset template to template

--- config/scripts/devpod.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-wget https://github.com/loft-sh/devpod/releases/latest/download/DevPod_linux_x86_64.tar.gz -O /tmp/devpod.tar.gz
-tar -xvf /tmp/devpod.tar.gz -C /
--- config/scripts/google-chrome.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-rpm-ostree install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
